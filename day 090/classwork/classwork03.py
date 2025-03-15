362d8c9da682445cd4d5b7af39876cd7901995e4
andriaalavidze <157210385+andriaalavidze@users.noreply.github.com>
2025-03-16 01:11:19 +0400

    commit

---
+++ day 090/classwork/classwork03.py
@@ -0,0 +1,12 @@
+list1 = [1, 2, 3]
+list1.append(4)
+list1.remove(2)
+list1.sort()
+print(list1)
+
+tuple1 = (1, 2, 3)
+
+set1 = {1, 2, 3}
+set1.add(4)
+set1.remove(2)
+print(set1)
---
+++ day 090/homework/homework03.py
@@ -0,0 +1,2 @@
+list1 = [1, 2, 2, 3, 4, 4, 5]
+print(set(list1))
